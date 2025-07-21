1f72644c91a5f3324c2fdfb7d382d08b0f2c675f
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-07-21 14:29:06 +0200

    added hover card

--- src/components/cards/NavCard.tsx
+++ src/components/cards/NavCard.tsx
@@ -1,7 +1,8 @@
 import { NavItem } from '@/types'
 import React from 'react'
-import { Button } from '../ui/button'
+import { Button, buttonVariants } from '../ui/button'
 import Link from 'next/link'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '../ui/hover-card'
 
 type NavCardProps = {
     item: NavItem
@@ -10,12 +11,21 @@ type NavCardProps = {
 function NavCard({ item }: NavCardProps) {
   return (
     <>
-        {item.isLink && (
+        {item.isLink ? (
             <Button className='py-0 text-foreground hover:text-foreground2 text-lg' variant={"link"} asChild>
                 <Link href={item.href}>
                     {item.label}
                 </Link>
             </Button>
+        ) : (
+            <HoverCard>
+                <HoverCardTrigger className='py-0 text-foreground hover:text-foreground2 text-lg font-medium hover:cursor-default'>
+                    Menu
+                </HoverCardTrigger>
+                <HoverCardContent>
+                    Content
+                </HoverCardContent>
+            </HoverCard>
         )}
     </>
   )
