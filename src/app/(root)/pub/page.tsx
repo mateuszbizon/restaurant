4ab12c157e1950e6c6067988e5768a24a024241f
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-07-22 10:44:46 +0200

    added pub page

---
+++ src/app/(root)/pub/page.tsx
@@ -0,0 +1,14 @@
+import PubSection from '@/components/sections/PubSection'
+import SubPageHero from '@/components/sections/SubPageHero'
+import React from 'react'
+
+function PubPage() {
+  return (
+    <>
+        <SubPageHero title='Pub' />
+        <PubSection />
+    </>
+  )
+}
+
+export default PubPage
