15a97380a881995460b647f33764591a955f7264
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-07-24 16:00:14 +0200

    added gsap

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,20 +1,53 @@
+"use client"
+
 import React from 'react'
 import Container from '../ui/container'
 import { Button } from '../ui/button'
 import Shadow from '../ui/shadow'
 import Link from 'next/link'
+import { gsap } from "gsap"
+import { useGSAP } from "@gsap/react"
 
 function Hero() {
+    useGSAP(() => {
+        gsap.fromTo(".heading", {
+            opacity: 0,
+            y: -50,
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1
+        })
+        gsap.fromTo(".text", {
+            opacity: 0,
+            x: -50
+        }, {
+            opacity: 1,
+            x: 0,
+            delay: 0.5,
+            duration: 1
+        })
+        gsap.fromTo(".buttons", {
+            opacity: 0,
+            y: 50
+        }, {
+            opacity: 1,
+            y: 0,
+            delay: 1,
+            duration: 1
+        })
+    }, [])
+
   return (
     <header className='py-section-padding bg-[url(/restaurant.jpg)] bg-cover bg-center bg-no-repeat relative z-0 h-screen'>
         <Shadow />
         <Container>
             <div className='space-y-5 h-full flex flex-col justify-center max-w-[800px]'>
-                <h1 className='heading1'>Restauracja & Pub</h1>
-                <p className='bigger-text'>
+                <h1 className='heading heading1'>Restauracja & Pub</h1>
+                <p className='text bigger-text'>
                     Zapraszamy do miejsca, gdzie wyśmienita kuchnia spotyka się z wyjątkowym klimatem pubu. Poczuj domową atmosferę, spróbuj lokalnych specjałów i odpocznij przy barze.
                 </p>
-                <div className='flex gap-5 flex-wrap'>
+                <div className='buttons flex gap-5 flex-wrap'>
                     <Button size={"lg"} className='text-lg' asChild>
                         <Link href={"/menu"}>
                             Nasze menu
--- src/components/sections/MenuSection.tsx
+++ src/components/sections/MenuSection.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import Container from '../ui/container'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs'
@@ -6,21 +8,43 @@ import Salads from '../common/Salads'
 import Pastas from '../common/Pastas'
 import Snacks from '../common/Snacks'
 import Desserts from '../common/Desserts'
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
 
 function MenuSection() {
+    useGSAP(() => {
+        gsap.fromTo(".card-animate", {
+            opacity: 0,
+            y: 50,
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 0.5,
+            stagger: 0.3,
+            scrollTrigger: {
+                trigger: "#menu",
+                start: "top center",
+                once: true
+            }
+        })
+    }, [])
+
   return (
-    <section className='py-section-padding'>
+    <section id='menu' className='py-section-padding'>
         <Container>
             <h2 className='heading2 text-center heading-margin-bottom'>Nasze menu</h2>
             <Tabs className='lg:flex-row gap-10' defaultValue='pizza'>
-                <TabsList className='bg-background rounded-xl border p-7 w-full lg:w-[400px] flex-col gap-5'>
+                <TabsList className='card-animate bg-background rounded-xl border p-7 w-full lg:w-[400px] flex-col gap-5'>
                     <TabsTrigger value='pizza'>Pizze</TabsTrigger>
                     <TabsTrigger value='salad'>Sałatki</TabsTrigger>
                     <TabsTrigger value='pasta'>Makarony</TabsTrigger>
                     <TabsTrigger value='snack'>Przekąski</TabsTrigger>
                     <TabsTrigger value='dessert'>Desery</TabsTrigger>
                 </TabsList>
-                <div className='grow bg-background rounded-xl border p-7'>
+                <div className='card-animate grow bg-background rounded-xl border p-7'>
                     <TabsContent value='pizza'>
                         <Pizzas />
                     </TabsContent>
