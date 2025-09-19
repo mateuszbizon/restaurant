497a6e6eb35943aa74d7b2fa74e7c15eb79f232e
Mateusz Bizoń <mat-biz@wp.pl>
2025-09-19 15:48:35 +0200

    added images

---
+++ src/constants/images.ts
@@ -0,0 +1,31 @@
+import { Image } from "@/types";
+
+export const BILARD_IMG: Image = {
+    src: "/bilard.jpg",
+    alt: "Stół bilardowy",
+}
+
+export const DRINK_IMG: Image = {
+    src: "/drink.webp",
+    alt: "Drink",
+}
+
+export const FOOD_IMG: Image = {
+    src: "/food.png",
+    alt: "Jedzenie",
+}
+
+export const GLOVO_IMG: Image = {
+    src: "/glovo.webp",
+    alt: "Glovo",
+}
+
+export const KARAOKE_IMG: Image = {
+    src: "/karaoke.jpg",
+    alt: "Karaoke",
+}
+
+export const RESTAURANT_IMG: Image = {
+    src: "/restaurant.jpg",
+    alt: "Restauracja",
+}
