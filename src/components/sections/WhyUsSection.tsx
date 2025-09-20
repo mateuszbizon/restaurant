a52fa214164bf7ea35f7c4b73048976dab504817
Mateusz Bizoń <mat-biz@wp.pl>
2025-09-20 17:28:53 +0200

    changed why us section grid and added image to that section

--- src/components/sections/WhyUsSection.tsx
+++ src/components/sections/WhyUsSection.tsx
@@ -2,6 +2,8 @@ import React, { ComponentProps } from 'react'
 import Container from '../ui/container'
 import { Gamepad2, Smile, Users, UtensilsCrossed } from 'lucide-react'
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import { RESTAURANT_IMG } from '@/constants/images'
 
 type WhyUsSectionProps = ComponentProps<'section'>
 
@@ -9,36 +11,61 @@ function WhyUsSection({ className, ...props }: WhyUsSectionProps) {
   return (
     <section className={cn('pb-section-padding bg-foreground', className)} {...props}>
         <Container>
-            <div className='bg-background-dark rounded-2xl p-5 md:p-10'>
-                <h2 className='heading2 text-center heading-margin-bottom'>Dlaczego my?</h2>
-                <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 text-center'>
-                    <div className='space-y-5'>
-                        <UtensilsCrossed className='size-10 mx-auto' />
-                        <h3 className='heading3 md:min-h-[120px]'>Świetna kuchnia, uczciwe ceny</h3>
-                        <p className='little-bigger-text'>
-                            Serwujemy pyszne dania ze świeżych składników, bez zbędnych udziwnień i za rozsądną cenę. U nas jakość idzie w parze ze smakiem.
-                        </p>
+            <div className='bg-background rounded-2xl p-5 md:p-10'>
+                <div className='grid lg:grid-cols-2 gap-10'>
+                    <div>
+                        <h2 className='heading2 mb-10'>Dlaczego my?</h2>
+                        <div className='space-y-10'>
+                            <div className='flex flex-col lg:flex-row gap-5'>
+                                <div className='rounded-md p-3 text-black bg-[#5DECBF] flex justify-center items-center h-fit mx-auto'>
+                                    <UtensilsCrossed className='size-8' />
+                                </div>
+                                <div className='space-y-2 text-center lg:text-left'>
+                                    <h3 className='heading3'>Świetna kuchnia, uczciwe ceny</h3>
+                                    <p className='little-bigger-text'>
+                                        Serwujemy pyszne dania ze świeżych składników, bez zbędnych udziwnień i za rozsądną cenę. U nas jakość idzie w parze ze smakiem.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='flex flex-col lg:flex-row gap-5'>
+                                <div className='rounded-md p-3 text-black bg-[#D38BC7] flex justify-center items-center h-fit mx-auto'>
+                                    <Users className='size-8' />
+                                </div>
+                                <div className='space-y-2 text-center lg:text-left'>
+                                    <h3 className='heading3'>Klimat, do którego chce się wracać</h3>
+                                    <p className='little-bigger-text'>
+                                        Luźna, przyjazna atmosfera — idealna na spotkania z paczką, randkę czy szybkie piwo po pracy. Po prostu dobre miejsce na dobry wieczór.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='flex flex-col lg:flex-row gap-5'>
+                                <div className='rounded-md p-3 text-black bg-[#00B38A] flex justify-center items-center h-fit mx-auto'>
+                                    <Gamepad2 className='size-8' />
+                                </div>
+                                <div className='space-y-2 text-center lg:text-left'>
+                                    <h3 className='heading3'>Rozrywka dla każdego</h3>
+                                    <p className='little-bigger-text'>
+                                        Bilard, rzutki, karaoke i ping pong – u nas nie da się nudzić. Mamy coś na spokojny wieczór i na konkretną imprezę
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='flex flex-col lg:flex-row gap-5'>
+                                <div className='rounded-md p-3 text-black bg-[#F9F871] flex justify-center items-center h-fit mx-auto'>
+                                    <Smile className='size-8' />
+                                </div>
+                                <div className='space-y-2 text-center lg:text-left'>
+                                    <h3 className='heading3'>Obsługa, która naprawdę się stara</h3>
+                                    <p className='little-bigger-text'>
+                                        Zespół z pasją i luzem — nie traktujemy Cię jak kolejny stolik, tylko jak gościa, który ma się dobrze bawić. Po prostu: fajni ludzie, dobra energia.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className='space-y-5'>
-                        <Users className='size-10 mx-auto' />
-                        <h3 className='heading3 md:min-h-[120px]'>Klimat, do którego chce się wracać</h3>
-                        <p className='little-bigger-text'>
-                            Luźna, przyjazna atmosfera — idealna na spotkania z paczką, randkę czy szybkie piwo po pracy. Po prostu dobre miejsce na dobry wieczór.
-                        </p>
-                    </div>
-                    <div className='space-y-5'>
-                        <Gamepad2 className='size-10 mx-auto' />
-                        <h3 className='heading3 md:min-h-[120px]'>Rozrywka dla każdego</h3>
-                        <p className='little-bigger-text'>
-                            Bilard, rzutki, karaoke i muzyka na żywo – u nas nie da się nudzić. Mamy coś na spokojny wieczór i na konkretną imprezę.
-                        </p>
-                    </div>
-                    <div className='space-y-5'>
-                        <Smile className='size-10 mx-auto' />
-                        <h3 className='heading3 md:min-h-[120px]'>Obsługa, która naprawdę się stara</h3>
-                        <p className='little-bigger-text'>
-                            Zespół z pasją i luzem — nie traktujemy Cię jak kolejny stolik, tylko jak gościa, który ma się dobrze bawić. Po prostu: fajni ludzie, dobra energia.
-                        </p>
+                    <div className='hidden lg:block pt-10'>
+                        <figure className='relative w-full max-w-[600px] mx-auto aspect-square'>
+                            <Image src={RESTAURANT_IMG.src} alt={RESTAURANT_IMG.alt} fill className='object-cover rounded-2xl' />
+                        </figure>
                     </div>
                 </div>
             </div>
